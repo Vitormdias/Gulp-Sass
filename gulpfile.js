e7f6ae0b76396d9c54cc2fbd70f8cfcf75dcb032
vitormdias <vitormd97@gmail.com>
2016-12-17 10:22:16 -0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+
+const config = {
+    srcPath: 'src/',
+    distPath: 'dist/'
+};
+
+gulp.task('browser-sync' , () => {
+    browserSync.init({
+        server: {
+          baseDir: './',
+        },
+        port: 8080,
+        startPath: 'index.html',
+    })
+});
+
+gulp.task('sass' , () => {
+    return gulp.src(config.srcPath + 'sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(config.distPath + 'css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('watch' , () => {
+    gulp.watch(config.srcPath+'sass/**/*.+(scss|sass)', ['sass']);
+});
